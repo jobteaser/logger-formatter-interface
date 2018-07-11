9b105d65419e6b5b41408d445fa7d18ca1428394
Gearnode <friminb@gmail.com>
2018-07-11 14:36:38 +0200

    Add first library implementation

--- lib/json_logger.ex
+++ lib/json_logger.ex
@@ -1,18 +1,36 @@
 defmodule Logger.Formatter.JSON do
   @moduledoc """
   Documentation for Logger.Formatter.JSON.
+   + add application env in logger config
+   + compile time error without configuration
   """
 
-  @doc """
-  Hello world.
+  # Raise compile error when no logger was configured. This avoid building
+  # production application without JSON encoder.
+  # TODO: fail condition only in use
+  Application.fetch_env!(:logger, :json_encoder)
 
-  ## Examples
+  @doc "Formats a log message as a JSON encoded string."
+  @spec format(
+          Logger.level(),
+          Logger.message(),
+          Logger.Formatter.time(),
+          keyword()
+        ) :: IO.chardata()
+  # Avoid fail in the formatting function.
+  def format(level, message, time, metadata) do
+    # This order avoid metadata overiding the default fields.
+    Map.new(metadata)
+    |> Map.merge(%{"msg" => message, "level" => level, "ts" => fmt_dt(time)})
+    |> Application.fetch_env!(:logger, :json_encoder).encode!()
+  rescue
+    _ ->
+      """
+      {"msg": "could not format: #{inspect({level, message, time, metadata})}"}
+      """
+  end
 
-      iex> Logger.Formatter.JSON.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defp fmt_dt({date, time}) do
+    "#{Logger.Formatter.format_date(date)} #{Logger.Formatter.format_time(time)}"
   end
 end
--- test/json_logger_test.exs
+++ test/json_logger_test.exs
@@ -1,8 +1,57 @@
+defmodule DummyEncoderSuccess do
+  def encode!(_), do: ""
+end
+
+defmodule DummyEncoderFail do
+  def encode!(_), do: raise("foo")
+end
+
+defmodule EncoderExpecter do
+  def encode!(map) do
+    if map["ts"] != "2014-12-30 12:06:30.100", do: raise "ts not ok"
+    if map["msg"] != "hello world", do: raise "msg not ok"
+    if map["level"] != :error, do: raise "level not ok"
+    if map[:foo] == nil, do: raise "foo not ok"
+    ""
+  end
+end
+
 defmodule Logger.Formatter.JSONTest do
   use ExUnit.Case
   doctest Logger.Formatter.JSON
 
-  test "greets the world" do
-    assert Logger.Formatter.JSON.hello() == :world
+  alias Logger.Formatter.JSON
+
+  test "work" do
+    Application.put_env(:logger, :json_encoder, DummyEncoderSuccess)
+
+    assert JSON.format(nil, nil, nil, nil) ==
+             "{\"msg\": \"could not format: {nil, nil, nil, nil}\"}\n"
+
+    assert JSON.format(:error, nil, nil, nil) ==
+             "{\"msg\": \"could not format: {:error, nil, nil, nil}\"}\n"
+
+    assert JSON.format(:error, "hello world", nil, nil) ==
+             "{\"msg\": \"could not format: {:error, \"hello world\", nil, nil}\"}\n"
+
+    ts = {{2014, 12, 30}, {12, 6, 30, 100}}
+
+    assert JSON.format(:error, "hello world", ts, nil) ==
+             "{\"msg\": \"could not format: {:error, \"hello world\", #{inspect(ts)}, nil}\"}\n"
+
+    assert JSON.format(:error, "hello world", ts, []) == ""
+
+    assert JSON.format(:error, "hello world", ts, foo: "bar") == ""
+
+    Application.put_env(:logger, :json_encoder, DummyEncoderFail)
+
+    assert JSON.format(:error, "hello world", ts, foo: "bar") ==
+             "{\"msg\": \"could not format: {:error, \"hello world\", {{2014, 12, 30}, {12, 6, 30, 100}}, [foo: \"bar\"]}\"}\n"
+
+    Application.put_env(:logger, :json_encoder, EncoderExpecter)
+
+    assert JSON.format(:error, "hello world", ts, foo: "bar") == ""
+
+    assert JSON.format(:error, "hello world", ts, foo: "bar", ts: "foobar") == ""
   end
 end
