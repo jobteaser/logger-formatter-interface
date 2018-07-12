5f52505fe1ebfdabf8dcf6686f88e55e9e557159
Gearnode <friminb@gmail.com>
2018-07-12 15:48:44 +0200

    Update mix.exs informtations

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Logger.Formatter.Encoder.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "Logger.Formatter.Encoder",
+      description: "A simple logger interface for Elixir console backend"
+      source_url: "https://github.com/jobteaser/logger-formatter-interface",
+      homepage_url: "http://github.com/jobteaser/logger-formatter-interface",
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -17,5 +22,15 @@ defmodule Logger.Formatter.Encoder.MixProject do
 
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      name: "logger_interface",
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/jobteaser/logger-formatter-interface"},
+      maintainers: ["Gearnode <friminb@gmail.com>", "Yann Very <yann.very@joteaser.com>"]
+    ]
   end
 end
